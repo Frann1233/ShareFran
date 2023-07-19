e31ab656c1aade97b0741e9fbb6b3c8150e67685
Frann1233 <franbusic8@gmail.com>
2023-07-19 20:53:59 +0200

    First Commit

---
+++ shareme_frontend/src/components/Spinner.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Dna } from "react-loader-spinner";
+
+const Spinner = ({ message }) => {
+  return (
+    <div className="flex flex-col justify-center items-center w-full h-full">
+      <Dna
+        visible={true}
+        height="60"
+        width="100"
+        ariaLabel="dna-loading"
+        wrapperStyle={{}}
+        wrapperClass="dna-wrapper"
+        className="m-5"
+      />
+
+      <p className="text-lg text-center px-2">{message}</p>
+    </div>
+  );
+};
+
+export default Spinner;
